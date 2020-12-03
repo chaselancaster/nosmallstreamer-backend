bce544f6cda71df8827bb89631e9780c4355fce4
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-03 13:56:11 -0800

    add morgan

--- app.js
+++ app.js
@@ -1,28 +1,43 @@
 const express = require('express');
+const { networkInterfaces } = require('os');
+const { nextTick } = require('process');
 
 // express app
 const app = express();
 
 // listen for requests
-app.listen(3000)
+app.listen(3000);
+
+app.use((req, res, next) => {
+  console.log('new request made:')
+  console.log('host: ', req.hostname)
+  console.log('path: ', req.path)
+  console.log('method: ', req.method)
+  next();
+})
+
+// register view engine
+app.set('view engine', 'ejs');
+// app.set('views', 'myviews');
 
 app.get('/', (req, res) => {
-    // res.send('<p>home page</p>')
-    res.sendFile('./views/index.html', { root: __dirname })
-})
+  const blogs = [
+    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
+  ];
+  res.render('index', { title: 'Home', blogs });
+});
+
 app.get('/about', (req, res) => {
-    // res.send('<p>about page</p>')
-    res.sendFile('./views/about.html', { root: __dirname })
-})
+  res.render('about', { title: 'About' });
+});
 
-// redirects
+app.get('/blogs/create', (req, res) => {
+  res.render('create', { title: 'Create a new blog' });
+});
 
-app.get('/about-us', (req, res) => {
-  res.redirect('/about')  
-})
-
-// 404 page 
-
+// 404 page
 app.use((req, res) => {
-    res.status(404).sendFile('./views/404.html', { root: __dirname })
-})
+  res.status(404).render('404', { title: '404' });
+});
