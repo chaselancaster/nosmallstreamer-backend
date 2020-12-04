1260810009d2beb53f2672dd7e6c6407f5f02174
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-04 14:27:17 -0800

    remove old blog import

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
-const blogRoutes = require('./routes/blogRoutes')
 
 // express app
 const app = express();
