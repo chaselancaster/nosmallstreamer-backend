8ffe95dad9a470c40bca0ef90cc14d9da304a3a4
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-04 14:39:50 -0800

    add all packages and middleware to app.js

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require('express');
+const session = requires('express-session');
 const morgan = require('morgan');
 const mongoose = require('mongoose');
+const createError = require('http-errors');
 
 // express app
 const app = express();
 
 // middleware
 app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(session({
+  secret: 'chase',
+  resave: false,
+  saveUninitialized: false
+}))
 app.use(morgan('dev'));
 
 // routes
