75b914d9320058b01b5be1d135c6ce1a87163d51
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-04 14:24:37 -0800

    clean up backend for nss use

--- app.js
+++ app.js
@@ -1,41 +1,17 @@
 const express = require('express');
 const morgan = require('morgan');
+const mongoose = require('mongoose');
+const blogRoutes = require('./routes/blogRoutes')
 
 // express app
 const app = express();
 
-// connect to mongoDB
-const dbURI = 'mongodb+srv://chase:chase@cluster0.cvvl3.mongodb.net/node-tuts?retryWrites=true&w=majority'
-
-// listen for requests
-app.listen(3000);
-
 // middleware & static files
-app.use(express.static('public'))
-app.use(morgan('dev'))
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(morgan('dev'));
 
-// register view engine
-app.set('view engine', 'ejs');
-// app.set('views', 'myviews');
-
-app.get('/', (req, res) => {
-  const blogs = [
-    {title: 'Yoshi finds eggs', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-    {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
-  ];
-  res.render('index', { title: 'Home', blogs });
-});
-
-app.get('/about', (req, res) => {
-  res.render('about', { title: 'About' });
-});
-
-app.get('/blogs/create', (req, res) => {
-  res.render('create', { title: 'Create a new blog' });
-});
+// routes
+app.use('/blogs', blogRoutes);
 
 // 404 page
-app.use((req, res) => {
-  res.status(404).render('404', { title: '404' });
-});
