91785d0b288135e54116f1619bc7816a54f47b8e
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-08 14:31:13 -0800

    add the rest of the register route

--- routes/users.js
+++ routes/users.js
@@ -4,8 +4,20 @@ const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
 
-router.post('/register', async (req, res) => {
+router.post('/register', async (req, res, next) => {
     console.log('register route hit')
+    try {
+        const createdUser = await User.create(req.body)
+        if (createdUser) {
+            req.session.dbId = createdUser._id
+            res.json({
+                user: createdUser,
+                success: true
+            })
+        }
+    } catch (err) {
+        res.json({ err })
+    }
 })
 
 module.exports = router;
