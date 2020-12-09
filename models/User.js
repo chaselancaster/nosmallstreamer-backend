d732ef8b0a07adcf363c3c6e725a6bdb4383a2b7
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-08 16:04:12 -0800

    actually remove username from UserSchema this time.

--- models/User.js
+++ models/User.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
-    username: { type: String, required: true },
+    email: { type: String, unique: true },
     password: { type: String, required: true },
     games: [Object]
 });
