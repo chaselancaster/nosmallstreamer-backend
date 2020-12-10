b68a2a04ffb199b6f44d90b241849abc642da2b8
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-10 11:00:09 -0800

    add api.js to routes folder

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const { body, validationResult } = require('express-validator');
 const bcrypt = require('bcryptjs');
 
 const User = require('../models/User');
@@ -8,23 +9,27 @@ const User = require('../models/User');
 router.post('/register', async (req, res, next) => {
     console.log('register route hit')
     try {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json({ errors: errors.array() });
+        }
         const { name, password, email } = req.body
         if (!name) {
             return res.send({
               success: false,
-              message: 'Name cannot be blank.'
+              message: 'Name can not be blank.'
             });
           }
         if (!email) {
             return res.send({
               success: false,
-              message: 'Email cannot be blank.'
+              message: 'Email can not be blank.'
             });
           }
         if (!password) {
             return res.send({
               success: false,
-              message: 'Password cannot be blank.'
+              message: 'Password can not be blank.'
             });
           }
         
@@ -101,5 +106,45 @@ router.put('/update/:id', async (req, res) => {
         console.log (err, '<- err in update user')
     }
 })
+
+// Update user w/ express-validator
+// router.put('/update/:id', [body('email').notEmpty(), body('password').notEmpty()], async (req, res) => {
+//     const userId = req.session.dbId ||req.params.id
+//     try {
+//         // Checking for validation errors
+//         const errors = validationResult(req);
+//         if (!errors.isEmpty()) {
+//             return res.status(400).json({ errors: errors.array() });
+//         }
+//         const user = await User.findById(userId)
+//         console.log(user, '<- user in update route')
+//         // Checking if any field is empty and setting the req.body variable to keep the old info
+//         // if (req.body.password === "") {
+//         //     req.body.password = user.password;
+//         //     console.log(req.body.password, "<- password when field is blank")
+//         // } else if (req.body.email === "") {
+//         //     req.body.email = user.email;
+//         //     console.log(req.body.email, "<- email when field is blank")
+//         // } else if (req.body.password === "" && req.body.email === "") {
+//         //     res.json({
+//         //         message: 'Fill in at least one field to update account.'
+//         //     })
+//         // }
+//         // Hashing password
+//         if (req.body.password !== "" && req.body.password !== user.password) {
+//             req.body.password = user.hashPassword(req.body.password)
+//             console.log(req.body.password, '<- password after hashed')
+//         }
+//         const updatedUser = await User.findByIdAndUpdate(userId, req.body, {
+//             new: true
+//         })
+//         console.log(updatedUser, '<- updatedUser')
+//         res.json({
+//             updatedUser
+//         })
+//     } catch (err) {
+//         console.log (err, '<- err in update user')
+//     }
+// })
 
 module.exports = router;
