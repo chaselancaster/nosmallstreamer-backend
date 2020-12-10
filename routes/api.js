6e5b6bb3afba598ff0157e961e3f789cd4697c6b
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-10 11:01:25 -0800

    add test get route in api.js

--- routes/api.js
+++ routes/api.js
@@ -2,3 +2,8 @@ const express = require('express');
 const router = express.Router();
 const fetch = require('node-fetch');
 
+router.get('/:name', async (req, res) => {
+    console.log('api name route hit')
+})
+
+module.exports = router;
