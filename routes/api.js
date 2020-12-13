0fa905988869769829963ef44698fcaec29524d8
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-13 12:14:15 -0800

    hide client id and secret in token fetch call

--- routes/api.js
+++ routes/api.js
@@ -5,7 +5,7 @@ const fetch = require('node-fetch');
 router.post('/:name', async (req, res) => {
     console.log('name route it')
     try {
-        const tokenCall = await fetch('https://id.twitch.tv/oauth2/token?client_id=wwwoxzpshnwpnko2nehgbmc0c52r82&client_secret=sk9s9er4zjzxr36zyy58lmtl0077h7&grant_type=client_credentials', { method: 'POST' 
+        const tokenCall = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=client_credentials`, { method: 'POST' 
         })
         const parsedToken = await tokenCall.json();
         const token = parsedToken.access_token
@@ -21,15 +21,10 @@ router.post('/:name', async (req, res) => {
         console.log(parsedGame, '<- parsedGame')
         const gameId = parsedGame.data[0].id
         console.log(gameId, '<- gameId')
-        const tokenCall2 = await fetch('https://id.twitch.tv/oauth2/token?client_id=wwwoxzpshnwpnko2nehgbmc0c52r82&client_secret=sk9s9er4zjzxr36zyy58lmtl0077h7&grant_type=client_credentials', { method: 'POST' 
-        })
-        const parsedToken2 = await tokenCall2.json()
-        const token2 = parsedToken2.access_token
-        console.log(parsedToken2, '<-- parsedToken2')
         // const streams = await fetch(`https://api.twitch.tv/helix/streams?game_id=${gameId}&first=100`, {
         //     method: 'GET',
         //     headers: {
-        //         'Authorization': `Bearer ${token2}`,
+        //         'Authorization': `Bearer ${token}`,
         //         'Cliend-Id': process.env.CLIENT_ID,
         //     }
         // })
