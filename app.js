e5d6d1cbc5dcb43d3bacc98ea1fb8d3ae98d274a
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-20 16:18:44 -0800

    add a get route to see if it works

--- app.js
+++ app.js
@@ -32,6 +32,10 @@ app.use(morgan('dev'));
 app.use('/users', usersRouter);
 app.use('/api', apiRouter);
 
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
 // Catching error
 app.use((req, res, next) => {
   next(createError(404));
