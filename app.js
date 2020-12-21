dcd5d78a937e3494f8df8545a37a68889fb9a0a8
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-20 16:12:48 -0800

    add or statement in app.listen

--- app.js
+++ app.js
@@ -37,7 +37,7 @@ app.use((req, res, next) => {
   next(createError(404));
 })
 
-app.listen(PORT, err => {
+app.listen(PORT || 3001, err => {
   console.log(err || 'App is listening on port, ', PORT)
 })
 
