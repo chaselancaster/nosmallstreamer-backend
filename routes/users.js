080a1d730c258249ff94f0660e1c804fc9173b16
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-23 10:32:31 -0800

    add jwt token to the register route and send it to the front end

--- routes/users.js
+++ routes/users.js
@@ -35,7 +35,6 @@ router.post('/register', async (req, res, next) => {
               message: 'Password can not be blank.'
             });
           }
-        
         const createdUser = await User.create(req.body)
         if (createdUser) {
             req.session.dbId = createdUser._id
