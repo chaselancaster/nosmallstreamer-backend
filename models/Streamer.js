e95fbc40a58c5d557a418857c2ddbd779a3a77a5
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-28 17:55:42 -0800

    Add logic to find a streamer or create a streamer and update the score for the user in the back-end

--- models/Streamer.js
+++ models/Streamer.js
@@ -3,7 +3,7 @@ const bcrypt = require('bcryptjs');
 
 const StreamerSchema = new mongoose.Schema({
     name: { type: String },
-    score: { type: Number }
+    score: { type: Number, default: 0 }
 });
 
 module.exports = mongoose.model("Streamer", StreamerSchema);
--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -1,15 +1,51 @@
 const express = require('express');
+const { find } = require('../models/Streamer');
 const router = express.Router();
 
 require('dotenv').config()
 
 const Streamer = require('../models/Streamer');
 
-router.post('/submit/:name/:vote', (req, res) => {
+router.post('/submit/:name/:vote', async (req, res) => {
     console.log('vote route in back-end hit')
-    console.log(req.params, 'req.params')
-    // const { name, vote } = req.params;
-    // console.log(`${name}, ${vote}`)
+    try {
+        const { name, vote } = req.params;
+        const findStreamer = await Streamer.find({ name: name }).exec();
+        if (findStreamer.length === 0) {
+            const createdStreamer = await Streamer.create(req.params)
+            console.log(createdStreamer, '<- createdStreamer')
+            if (vote === "upvote") {
+                createdStreamer.score++
+                createdStreamer.save()
+                res.json({
+                    message: 'Upvote has been given'
+                })
+            } else if (vote === "downvote") {
+                createdStreamer.score--
+                createdStreamer.save()
+                res.json({
+                    message: 'Downvote has been given'
+                })
+            } 
+        } else {
+            if (vote === "upvote") {
+                findStreamer[0].score++
+                findStreamer[0].save()
+                res.json({
+                    message: 'Upvote has been given'
+                })
+            } else if (vote === "downvote") {
+                findStreamer[0].score--
+                findStreamer[0].save()
+                res.json({
+                    message: 'Downvote has been given'
+                })
+            } 
+            console.log(findStreamer, '<- findStreamer after votes')
+        }
+    } catch (err) {
+        console.log(err, '<- err in submit vote')
+    }
 })
 
 module.exports = router;
