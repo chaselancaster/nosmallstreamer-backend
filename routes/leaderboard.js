09c28a748bacf57083425cc59f5eb8839dfd0746
Chase Lancaster <chaseblancaster@gmail.com>
2020-12-30 13:42:01 -0800

    remove a couple console.logs from the leaderboard.js file

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -10,14 +10,26 @@ router.post('/submit/:name/:vote', async (req, res) => {
     console.log('vote route in back-end hit')
     try {
         const { name, vote } = req.params;
-        const findStreamer = await Streamer.find({ name: name }).exec();
-        // if (findStreamer.length === 0) {
-        //         findStreamer = await Streamer.create(req.params)
+        // let votedStreamer = await Streamer.find({ name: name }).exec();
+        // if (votedStreamer.length === 0) {
+        //         votedStreamer = await Streamer.create(req.params)
         // }
-        /* Now voting logic here!!!!!! */
+        // if (vote === "upvote") {
+        //     votedStreamer.score++
+        //     votedStreamer.save()
+        //     res.json({
+        //         message: 'Upvote has been given'
+        //     })
+        // } else if (vote === "downvote") {
+        //     votedStreamer.score--
+        //     votedStreamer.save()
+        //     res.json({
+        //         message: 'Downvote has been given'
+        //     })
+        // }
+        const findStreamer = await Streamer.find({ name: name }).exec();
         if (findStreamer.length === 0) {
             const createdStreamer = await Streamer.create(req.params)
-            console.log(createdStreamer, '<- createdStreamer')
             if (vote === "upvote") {
                 createdStreamer.score++
                 createdStreamer.save()
@@ -31,7 +43,6 @@ router.post('/submit/:name/:vote', async (req, res) => {
                     message: 'Downvote has been given'
                 })
             } 
-            /* !!!! * /
             /* else respond err */
         } else {
             if (vote === "upvote") {
@@ -47,7 +58,6 @@ router.post('/submit/:name/:vote', async (req, res) => {
                     message: 'Downvote has been given'
                 })
             } 
-            console.log(findStreamer, '<- findStreamer after votes')
         }
     } catch (err) {
         console.log(err, '<- err in submit vote')
