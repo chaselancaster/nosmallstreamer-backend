ecd21d81c782989daec6ba780ec38e4e9632f73e
Chase Lancaster <chaseblancaster@gmail.com>
2021-01-07 15:12:23 -0800

    add profile_image_url to the Streamer schema for further storage

--- models/Streamer.js
+++ models/Streamer.js
@@ -4,7 +4,8 @@ const bcrypt = require('bcryptjs');
 const StreamerSchema = new mongoose.Schema({
     name: { type: String },
     score: { type: Number, default: 0 },
-    user_id: { type: String }
+    user_id: { type: String },
+    profile_image_url: { type: String }
 });
 
 module.exports = mongoose.model("Streamer", StreamerSchema);
