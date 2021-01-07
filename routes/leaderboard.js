247e708ed4d7c2bd1584fc129be442d1c6e1e70f
Chase Lancaster <chaseblancaster@gmail.com>
2021-01-07 15:11:01 -0800

    Receive the streamer profile image from the getStreamerProfileImage func and pass it back to the leaderboard vote func

--- routes/leaderboard.js
+++ routes/leaderboard.js
@@ -7,31 +7,43 @@ require('dotenv').config()
 
 const Streamer = require('../models/Streamer');
 
-// const getStreamerInfo = async (id) => {
-//     const streamer = await fetch(`https://`)
-// }
+const getStreamerProfileImage = async (id) => {
+    const streamer = await fetch(`https://api.twitch.tv/helix/users?id=${id}`, {
+            method: 'GET',
+            headers: {
+                'Authorization': `Bearer ${process.env.ACCESS_TOKEN}`,
+                'Client-Id': process.env.CLIENT_ID
+            }
+        })
+    const parsedStreamer = await streamer.json()
+    // console.log(parsedStreamer, '<- parsedStreamer')
+    const profileImageUrl = parsedStreamer.data[0].profile_image_url
+    // console.log(profileImageUrl, "<- profileImageUrl")
+    return profileImageUrl
+}
 
 router.post('/submit/:name/:vote/:user_id', async (req, res) => {
     console.log('vote route in back-end hit')
     try {
         const { name, vote, user_id } = req.params;
-        console.log(user_id, '<- user_id')
+        // console.log(user_id, '<- user_id')
         const findStreamer = await Streamer.find({ name: name }).exec();
-        console.log(findStreamer, '<- findStreamer')
+        // console.log(findStreamer, '<- findStreamer')
+        const streamerProfileImage = await getStreamerProfileImage(user_id)
         if (findStreamer.length === 0) {
             const createdStreamer = await Streamer.create(req.params)
-            console.log(createdStreamer, '<- createdStreamer')
+            // console.log(createdStreamer, '<- createdStreamer')
             if (vote === "upvote") {
                 createdStreamer.score++
                 createdStreamer.save()
-                console.log(createdStreamer, '<- createdStreamer after upvote')
+                // console.log(createdStreamer, '<- createdStreamer after upvote')
                 res.json({
                     message: 'Upvote has been given'
                 })
             } else if (vote === "downvote") {
                 createdStreamer.score--
                 createdStreamer.save()
-                console.log(createdStreamer, '<- createdStreamer after downvote')
+                // console.log(createdStreamer, '<- createdStreamer after downvote')
                 res.json({
                     message: 'Downvote has been given'
                 })
@@ -41,14 +53,14 @@ router.post('/submit/:name/:vote/:user_id', async (req, res) => {
             if (vote === "upvote") {
                 findStreamer[0].score++
                 findStreamer[0].save()
-                console.log(findStreamer, '<- findStreamer after upvote')
+                // console.log(findStreamer, '<- findStreamer after upvote')
                 res.json({
                     message: 'Upvote has been given'
                 })
             } else if (vote === "downvote") {
                 findStreamer[0].score--
                 findStreamer[0].save()
-                console.log(findStreamer, '<- findStreamer after downvote')
+                // console.log(findStreamer, '<- findStreamer after downvote')
                 res.json({
                     message: 'Downvote has been given'
                 })
