117b229f981bd2f2cb5a5244ce93586f812a8f4f
Chase Lancaster <chaseblancaster@gmail.com>
2021-04-03 14:59:09 -0700

    add route to fetch token.

--- routes/api.js
+++ routes/api.js
@@ -1,82 +1,96 @@
-const express = require('express')
-const router = express.Router()
-const fetch = require('node-fetch')
-const jwt = require('jsonwebtoken')
+const express = require("express");
+const router = express.Router();
+const fetch = require("node-fetch");
+const jwt = require("jsonwebtoken");
 
-require('dotenv').config()
+require("dotenv").config();
 
 const authenticateToken = (req, res, next) => {
-  const authHeader = req.headers.authorization
-  const token = authHeader && authHeader.split(' ')[1]
-  if (token == null) return res.sendStatus(401)
+  const authHeader = req.headers.authorization;
+  const token = authHeader && authHeader.split(" ")[1];
+  if (token == null) return res.sendStatus(401);
 
   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-    if (err) return res.sendStatus(403)
-    req.user = user
-    next()
-  })
-}
+    if (err) return res.sendStatus(403);
+    req.user = user;
+    next();
+  });
+};
 
-router.get('/stream/:name/:viewers', authenticateToken, async (req, res) => {
+router.get("/stream/:name/:viewers", authenticateToken, async (req, res) => {
   try {
-    const { name, viewers } = req.params
+    const { name, viewers } = req.params;
     const game = await fetch(`https://api.twitch.tv/helix/games?name=${name}`, {
-      method: 'GET',
+      method: "GET",
       headers: {
         Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
-        'Client-Id': process.env.CLIENT_ID
+        "Client-Id": process.env.CLIENT_ID,
+      },
+    });
+    const parsedGame = await game.json();
+    const gameId = parsedGame.data[0].id;
+    const streams = await fetch(
+      `https://api.twitch.tv/helix/streams?game_id=${gameId}&first=100`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+          "Client-Id": process.env.CLIENT_ID,
+        },
       }
-    })
-    const parsedGame = await game.json()
-    const gameId = parsedGame.data[0].id
-    const streams = await fetch(`https://api.twitch.tv/helix/streams?game_id=${gameId}&first=100`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
-        'Client-Id': process.env.CLIENT_ID
-      }
-    })
-    const parsedStreams = await streams.json()
-    const cursor = parsedStreams.pagination.cursor
+    );
+    const parsedStreams = await streams.json();
+    const cursor = parsedStreams.pagination.cursor;
     const filteredStreams = parsedStreams.data.filter(
-      s => s.viewer_count <= viewers
-    )
+      (s) => s.viewer_count <= viewers
+    );
     res.json({
       streams: filteredStreams,
       success: true,
       gameId,
-      cursor
-    })
+      cursor,
+    });
   } catch (err) {
-    console.log(err, '<- err in stream call')
+    console.log(err, "<- err in stream call");
   }
-})
+});
 
-router.get('/more/:gameId/:cursor', async (req, res) => {
+router.get("/more/:gameId/:cursor", async (req, res) => {
   try {
-    const moreStreams = await fetch(`https://api.twitch.tv/helix/streams?game_id=${req.params.gameId}&after=${req.params.cursor}`, {
-      method: 'GET',
-      headers: {
-        Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
-        'Client-Id': process.env.CLIENT_ID
+    const moreStreams = await fetch(
+      `https://api.twitch.tv/helix/streams?game_id=${req.params.gameId}&after=${req.params.cursor}`,
+      {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${process.env.ACCESS_TOKEN}`,
+          "Client-Id": process.env.CLIENT_ID,
+        },
       }
-    })
-    const parsedMoreStreams = await moreStreams.json()
-    const cursor = parsedMoreStreams.pagination.cursor
+    );
+    const parsedMoreStreams = await moreStreams.json();
+    const cursor = parsedMoreStreams.pagination.cursor;
     res.json({
       moreStreams: parsedMoreStreams.data,
-      cursor
-    })
+      cursor,
+    });
   } catch (err) {
-    console.log(err, '<- err in load more route')
+    console.log(err, "<- err in load more route");
   }
-})
+});
 
 // Method to get Twitch token
-// const tokenCall = await fetch(`https://id.twitch.tv/oauth2/token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=client_credentials`, { method: 'POST'
-//  })
-//  const parsedToken = await tokenCall.json();
-//  const token = parsedToken
-// console.log(token, '<- token in Twitch call')
+router.get("/token", async (req, res) => {
+  try {
+    const tokenCall = await fetch(
+      `https://id.twitch.tv/oauth2/token?client_id=${process.env.CLIENT_ID}&client_secret=${process.env.CLIENT_SECRET}&grant_type=client_credentials`,
+      { method: "POST" }
+    );
+    const parsedToken = await tokenCall.json();
+    const token = parsedToken;
+    console.log(token, "<- token in Twitch call");
+  } catch {
+    console.log(err, "could not receive token");
+  }
+});
 
-module.exports = router
+module.exports = router;
